3be5f452320be5e357f7f60dc0fa9a902f944913
Zizaco Zizuini <zizaco@gmail.com>
2019-09-17 00:27:44 +0200

    chore: added boilerplate of typescript + webpack project

---
+++ webpack.config.dev.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "bundle.js",
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".glsl"],
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            { test: /\.glsl$/, loader: "webpack-glsl-loader" },
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: "public" },
+        ]),
+    ],
+    externals: {
+        // "babylonjs": "BABYLON",
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "./dist"),
+        compress: true,
+        host: '0.0.0.0',
+        port: 9000,
+    },
+}
