fd72c5064bc92ae9005746b1cc824f716ff437f4
Zizaco Zizuini <zizaco@gmail.com>
2019-09-18 00:51:37 +0200

    chore: tweaked way to debug app

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import { Game } from "./Game";
 import * as localforage from 'localforage';
+import * as BABYLON from "babylonjs";
 
 declare global {
   interface Window {
     game: Game
     localforage: LocalForage
+    BABYLON: any
   }
 }
 
 if (process.env.NODE_ENV === 'development') {
   window.localforage = localforage
+  window.BABYLON = BABYLON
 }
 
 const view = document.getElementById("view") as HTMLCanvasElement;
