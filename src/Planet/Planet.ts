26a382721dced373c6c0c41a3708c272b27c98eb
Zizaco Zizuini <zizaco@gmail.com>
2019-09-20 03:03:45 +0200

    cfeat: texture eneration leanup and resolution increase

--- src/Planet/Planet.ts
+++ src/Planet/Planet.ts
@@ -12,13 +12,14 @@ class Planet {
   mesh: PlanetMesh
 
   constructor(scene) {
-    const options = { diameter: 1, diameterX: 1, subdivisions: 256 }
+    const options = { diameter: 1, diameterX: 1, subdivisions: 512 }
 
     // this.mesh = BABYLON.MeshBuilder.CreateSphere("sphere", options, scene);
     this.mesh = new PlanetMesh('myPlanet', options as any, scene)
     // this.mesh.material = this.generateMaterialOld(scene)
     setTimeout(() => {
       this.mesh.material = this.generateMaterial(scene)
+      this.mesh.subdivisions = 30
     }, 100)
   }
 
@@ -26,25 +27,16 @@ class Planet {
    * New procedural material generation
    */
   protected generateMaterial(scene): BABYLON.Material {
-    const textureResolution = 512
+    const textureResolution = 2048
+    const TEX_RES = textureResolution
     const blueprint = {
       diff: new BABYLON.DynamicTexture("texture", textureResolution, scene, false),
       normal: new BABYLON.DynamicTexture("texture", textureResolution, scene, false),
     }
 
     const grassDrawContext = blueprint.diff.getContext();
-    const imageData = grassDrawContext.createImageData(512, 512);
     const openSimplex = new OpenSimplexNoise(Date.now());
-    // for (let x = 0; x < 512; x++) {
-    //   for (let y = 0; y < 512; y++) {
-    //     const i = (x + y * 512) * 4;
-    //     const value = (openSimplex.noise2D(x/10, y/10) + 1) * 128;
-    //     imageData.data[i] = 255;
-    //     imageData.data[i + 1] = 255-value;
-    //     imageData.data[i + 2] = 255-value;
-    //     imageData.data[i + 3] = 255;
-    //   }
-    // }
+
     const normals = this.mesh.planetMesh.getVerticesData(BABYLON.VertexBuffer.NormalKind)
     const uv = this.mesh.planetMesh.getVerticesData(BABYLON.VertexBuffer.UVKind)
 
@@ -53,28 +45,13 @@ class Planet {
 
     const numberOfVertices = normals.length / 3
     grassDrawContext.fillStyle = `rgb(128,128,128)`
-    grassDrawContext.fillRect(0, 0, 512, 512)
+    grassDrawContext.fillRect(0, 0, TEX_RES, TEX_RES)
     // grassDrawContext.filter = `blur(${pixelWidth/2}px)`;
     for (let i = 0; i < numberOfVertices; i++) {
       const [x, y, z] = [normals[i * 3], normals[i * 3 + 1], normals[i * 3 + 2]]
-      const value = (openSimplex.noise3D(x * 8, y * 8, z * 8) + 1) * 128;
+      const value = (openSimplex.noise3D(x * 256, y * 256, z * 256) + 1) * 128;
       grassDrawContext.fillStyle = `rgb(${value}, ${value}, ${value})`
-      // const isHorizontalSide = 512 * uv[i * 2 + 1] < (512 / 3) || 512 * uv[i * 2 + 1] > (512 / 3) * 2
-      // const ensureIsHorizontalSide = Math.floor(512 * uv[i * 2]) < Math.floor(512 / 4) || Math.floor(512 * uv[i * 2]) > Math.floor(512 / 4 * 2)
-      // const isFirstPixelOfLeftSide = Math.floor(512 * uv[i * 2]) === 128
-      // const isFirstPixelOfTopSide = Math.floor(512 * uv[i * 2+1]) === Math.floor(512/3)
-      // const isLastPixelOfTopSide = Math.floor(512 * uv[i * 2+1]) === Math.floor(512/3*2)
-      // if (isFirstPixelOfTopSide) {
-      //   grassDrawContext.fillRect(512 * uv[i * 2], 512 * uv[i * 2 + 1] -16, pixelWidth, pixelHeight+16)
-      // }
-      // if (isHorizontalSide && isFirstPixelOfLeftSide ) {
-      //   grassDrawContext.fillRect(512 * uv[i * 2]-16, 512 * uv[i * 2 + 1], pixelWidth+16, pixelHeight)
-      // }
-      // if (ensureIsHorizontalSide && isLastPixelOfTopSide) {
-      //   grassDrawContext.fillRect(512 * uv[i * 2], 512 * uv[i * 2 + 1], pixelWidth, pixelHeight+16)
-      // }
-      grassDrawContext.fillRect(512 * uv[i * 2] - pixelWidth / 2, 512 * (1 - uv[i * 2 + 1]) - pixelHeight / 2, pixelWidth, pixelHeight)
-
+      grassDrawContext.fillRect(TEX_RES * uv[i * 2] - pixelWidth / 2, TEX_RES * (1 - uv[i * 2 + 1]) - pixelHeight / 2, pixelWidth, pixelHeight)
     }
 
     // grassDrawContext.putImageData(imageData, 0, 0);
@@ -84,7 +61,7 @@ class Planet {
     // material.bumpTexture = new BABYLON.Texture("textures/planetNormal.png", scene)
     // material.diffuseColor = new BABYLON.Color3(0.8, 0.26, 0.4)
     material.specularColor = new BABYLON.Color3(0.2, 0.2, 0.2);
-    material.emissiveColor = new BABYLON.Color3(1, 1, 1);
+    // material.emissiveColor = new BABYLON.Color3(1, 1, 1);
     material.specularPower = 14
 
     return material
